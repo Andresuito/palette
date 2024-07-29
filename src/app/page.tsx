6161a600009569559822a815da0367f76fd19ffd
Andresuito <barrancorios@gmail.com>
2024-07-29 18:02:34 +0200

    chore: Add keyboard shortcut instructions for generating a new palette

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,12 @@ export default function Home() {
   return (
     <main className="h-full xl:overflow-y-hidden">
       <Navbar />
+      <section className="flex justify-between py-5 px-6">
+        <p>
+          Press <span className="font-medium">N</span> to generate a new
+          palette.
+        </p>
+      </section>
       <Palette />
       <footer className="block sm:hidden px-6 py-4">
         <p className="text-center text-neutral-800 dark:text-white">
