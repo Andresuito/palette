a722ef87af693204f8ecde6dd93f302aaa22dac7
Andresuito <barrancorios@gmail.com>
2024-07-30 12:56:32 +0200

    feat: Add GenerateNewPalette component for generating new palettes

---
+++ src/components/pallete/GenerateNewPalette.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import React from "react";
+import { Button } from "@/components/ui/button";
+import { usePalette } from "@/context/PaletteContext";
+
+function GenerateNewPalette() {
+  const { generateAndSaveNewPalette } = usePalette();
+
+  return (
+    <div>
+      <Button size="sm" variant="default" onClick={generateAndSaveNewPalette}>
+        Generate New
+      </Button>
+    </div>
+  );
+}
+
+export default GenerateNewPalette;
