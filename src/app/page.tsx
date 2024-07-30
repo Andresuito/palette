46884943cc88f5fa0ab8fa1d43f463071a9ea76e
Andresuito <barrancorios@gmail.com>
2024-07-30 12:55:57 +0200

    chore: Update page component to use new palette and configuration features

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,24 @@
 import React from "react";
 import Navbar from "@/components/Navbar";
-import Palette from "@/components/generator/Palette";
+import ConfigPallete from "@/components/pallete/ConfigPalette";
+import Palette from "@/components/pallete/Palette";
+import GenerateNewPalette from "@/components/pallete/GenerateNewPalette";
 
 export default function Home() {
   return (
     <main className="h-full xl:overflow-y-hidden">
       <Navbar />
       <section className="flex justify-between py-5 px-6">
-        <p>
-          Press <span className="font-medium">N</span> to generate a new
-          palette.
-        </p>
+        <GenerateNewPalette />
+        <ConfigPallete />
       </section>
       <Palette />
-      <footer className="block sm:hidden px-6 py-4">
-        <p className="text-center text-neutral-800 dark:text-white">
-          Made with ❤️ by <a href="https://www.andresbr.com/">Andres B.R</a>
+      <footer className="px-6 py-4">
+        <p className="text-start text-neutral-800 dark:text-white">
+          Made by{" "}
+          <a target="_blank" href="https://www.andresbr.com/">
+            Andres B.R
+          </a>
         </p>
       </footer>
     </main>
