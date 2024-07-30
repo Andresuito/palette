ed085475ac6c4e2ab3a90605221bde4215e9942d
Andresuito <barrancorios@gmail.com>
2024-07-30 14:57:54 +0200

    refactor: Update metadata and manifest for website SEO and branding

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,52 @@ export const metadata: Metadata = {
     default: "Palette",
     template: "%s | Palette",
   },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: false,
+      noimageindex: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  keywords: ["palette", "color", "design", "developer", "scheme"],
+  description:
+    "Discover and create beautiful color schemes for your projects. Perfect for designers and developers.",
+  openGraph: {
+    title: "Palette",
+    description:
+      "Discover and create beautiful color schemes for your projects. Perfect for designers and developers.",
+    url: "https://palette.andresbr.com",
+    type: "website",
+    images: [
+      {
+        url: "./opengraph-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Palette",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "@_andresbr",
+    title: "Palette",
+    description:
+      "Discover and create beautiful color schemes for your projects. Perfect for designers and developers.",
+    images: [
+      {
+        url: "https://palette.andresbr.com/opengraph-image.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Palette",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
