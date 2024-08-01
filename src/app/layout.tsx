71afefd6c9a05cb08b5c04d1b1d67b6bf5c0034b
Andresuito <barrancorios@gmail.com>
2024-08-01 15:36:50 +0200

    refactor: Update default theme and enableSystem in RootLayout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -74,8 +74,8 @@ export default function RootLayout({
       >
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
-          enableSystem
+          defaultTheme="light"
+          enableSystem={false}
           disableTransitionOnChange
         >
           <ThemeConfigProvider>
