74b65af9627d2c49fa59fd16e32de65a1312deb3
Andresuito <barrancorios@gmail.com>
2024-08-15 12:45:30 +0200

    refactor: Update generatePalettePDF function to support multiple color formats

--- src/utils/pdfGenerator.ts
+++ src/utils/pdfGenerator.ts
@@ -1,45 +1,63 @@
 import jsPDF from 'jspdf';
+import { hexToRgb, hexToRgba, hexToHsl, hexToHsb, hexToCmyk } from './colorUtils';
 
-export async function generatePalettePDF(palette: string[]): Promise<Uint8Array> {
+export async function generatePalettePDF(palette: { hex: string; name: string }[], formats: string[]): Promise<Uint8Array> {
   const doc = new jsPDF('l', 'mm', 'a4');
   const boxWidth = 60;
   const boxHeight = 60;
-  const margin = 8;
-  const borderRadius = 2;
-  const pageWidth = doc.internal.pageSize.width;
+  const margin = 15;
+  const borderRadius = 4;
   const pageHeight = doc.internal.pageSize.height;
-  const titleFontSize = 16;
+  const titleFontSize = 18;
   const textFontSize = 12;
+  const lineHeight = 8;
   let x = margin;
-  let y = margin + titleFontSize + 10;
+  let y = margin + titleFontSize + 20;
 
   const drawTitle = () => {
     doc.setFontSize(titleFontSize);
+    doc.setFont("helvetica", "bold");
+    doc.setTextColor(0, 0, 0);
     doc.text('palette', margin, margin + titleFontSize);
     doc.setFontSize(textFontSize);
+    doc.setFont("helvetica", "normal");
   };
 
   drawTitle();
 
-  palette.forEach((color) => {
-    doc.setFillColor(color);
+  const formatFunctions: { [key: string]: (hex: string) => string } = {
+    'HEX': (hex) => hex,
+    'RGB': (hex) => {
+      const { r, g, b } = hexToRgb(hex);
+      return `rgb(${r}, ${g}, ${b})`;
+    },
+    'RGBA': (hex) => hexToRgba(hex, 1),
+    'HSL': (hex) => hexToHsl(hex),
+    'HSB': (hex) => hexToHsb(hex),
+    'CMYK': (hex) => hexToCmyk(hex),
+  };
+
+  palette.forEach(({ hex }) => {
+    doc.setFillColor(hex);
     doc.roundedRect(x, y, boxWidth, boxHeight, borderRadius, borderRadius, 'F');
 
-    doc.setTextColor(0, 0, 0);
-    doc.text(color, x + 2, y + boxHeight + 10);
+    const formatsStartX = x + boxWidth + margin;
+    let formatsY = y + (boxHeight - (formats.length * lineHeight)) / 2;
+    doc.setFontSize(textFontSize);
+    formats.forEach((format) => {
+      const formattedColor = formatFunctions[format] ? formatFunctions[format](hex) : hex;
+      doc.setTextColor(50, 50, 50);
+      doc.text(`${format}: ${formattedColor}`, formatsStartX, formatsY);
+      formatsY += lineHeight;
+    });
 
-    x += boxWidth + margin;
+    y += Math.max(boxHeight, formatsY - y) + margin;
 
-    if (x + boxWidth > pageWidth - margin) {
+    if (y + boxHeight > pageHeight - margin) {
+      doc.addPage();
       x = margin;
-      y += boxHeight + 20;
-
-      if (y + boxHeight > pageHeight - margin) {
-        doc.addPage();
-        x = margin;
-        y = margin + titleFontSize + 10;
-        drawTitle();
-      }
+      y = margin + titleFontSize + 20;
+      drawTitle();
     }
   });
 
