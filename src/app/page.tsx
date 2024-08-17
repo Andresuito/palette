b15e6133e356a759809b4f8a6bf53aa9bce56868
Andresuito <barrancorios@gmail.com>
2024-08-17 14:18:32 +0200

    refactor: Remove footer from Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import GenerateNewPalette from "@/components/palette/GenerateNewPalette";
 
 export default function Home() {
   return (
-    <main className="h-full xl:overflow-y-hidden">
+    <main className="h-full">
       <section className="flex justify-between py-5 px-6">
         <GenerateNewPalette />
         <div className="flex space-x-3 items-center">
@@ -13,14 +13,6 @@ export default function Home() {
         </div>
       </section>
       <Palette />
-      <footer className="block xl:hidden px-6 py-4">
-        <p className="text-start text-neutral-800 dark:text-white">
-          Made by{" "}
-          <a target="_blank" href="https://www.andresbr.com/">
-            Andres B.R
-          </a>
-        </p>
-      </footer>
     </main>
   );
 }
