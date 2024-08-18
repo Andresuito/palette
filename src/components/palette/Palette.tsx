5a4ef5cfd1683b9e940ff47240ac813cb256ea8e
Andresuito <barrancorios@gmail.com>
2024-08-18 11:20:51 +0200

    refactor: Update Palette component to handle custom color input

--- src/components/palette/Palette.tsx
+++ src/components/palette/Palette.tsx
@@ -4,15 +4,47 @@ import React, { useEffect } from "react";
 import { usePalette } from "@/context/PaletteContext";
 import Spinner from "./Spinner";
 import ColorCard from "./ColorCard";
+import { useSearchParams, useRouter } from "next/navigation";
+import { findClosestColorName } from "@/utils/colorUtils";
 
 function Palette() {
-  const { colors, generateAndSaveNewPalette } = usePalette();
+  const { colors, setColors, generateAndSaveNewPalette } = usePalette();
+  const searchParams = useSearchParams();
+  const router = useRouter();
+
+  const colorsHexString = searchParams.get("") || "";
+  const colorsHex = colorsHexString.split("-");
 
   useEffect(() => {
-    if (colors.length === 0) {
+    const isValidColors =
+      colorsHex.length > 0 &&
+      colorsHex.every((color) => /^#?[0-9A-Fa-f]{6}$/.test(color));
+
+    if (isValidColors) {
+      const formattedColors = colorsHex.map((hex) => ({
+        hex: hex.startsWith("#") ? hex : `#${hex}`,
+        name: findClosestColorName(hex.startsWith("#") ? hex : `#${hex}`),
+        isPinned: false,
+      }));
+
+      if (JSON.stringify(formattedColors) !== JSON.stringify(colors)) {
+        setColors(formattedColors);
+      }
+
+      if (searchParams.get("") !== "") {
+        router.replace("/");
+      }
+    } else if (colors.length === 0) {
       generateAndSaveNewPalette();
     }
-  }, [generateAndSaveNewPalette, colors.length]);
+  }, [
+    colorsHex,
+    setColors,
+    generateAndSaveNewPalette,
+    colors,
+    router,
+    searchParams,
+  ]);
 
   if (colors.length === 0) {
     return <Spinner />;
